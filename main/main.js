0437a33d4f1910d51be4968db24271b0d36cbd7f
zhangge1998 <1263061442@qq.com>
2017-07-24 13:56:02 +0800

    printReceiptTxt and test

--- main/main.js
+++ main/main.js
@@ -52,3 +52,23 @@ let buildTotal = (itemTotals)=> {
     return {list: itemTotals, total: total, discount: discount};
 };
 
+let printReceiptTxt = (total)=> {
+    let dateDigitToString;
+    dateDigitToString = num => num < 10 ? `0${num}` : num;
+    let currentDate = new Date(),
+        year = dateDigitToString(currentDate.getFullYear()),
+        month = dateDigitToString(currentDate.getMonth() + 1),
+        date = dateDigitToString(currentDate.getDate()),
+        hour = dateDigitToString(currentDate.getHours()),
+        minute = dateDigitToString(currentDate.getMinutes()),
+        second = dateDigitToString(currentDate.getSeconds()),
+        formattedDateString = `${year}年${month}月${date}日 ${hour}:${minute}:${second}`;
+    let string = '***<没钱赚商店>收据***';
+    string+='\n打印时间：'+formattedDateString+'\n----------------------';
+    for (let object of total.list) {
+        string += '\n' + '名称：' + object.cartItem.item.name + '，' + '数量：' + object.cartItem.count + object.cartItem.item.unit + '，' + '单价：' + object.cartItem.item.price.toFixed(2) + '(元)' + '，' + '小计：' + object.subtotal.toFixed(2) + '(元)';
+    }
+    string += '\n' + '----------------------' + '\n' + '总计：' + total.total.toFixed(2) + '(元)' + '\n' + '节省：' + total.discount.toFixed(2) + '(元)' + '\n' + '**********************';
+    return string;
+};
+
--- test/main-test.js
+++ test/main-test.js
@@ -133,3 +133,59 @@ describe('buildTotal', ()=> {
         expect(buildTotal(itemTotals)).toEqual(total);
     })
 });
+describe('printReceiptTxt', () => {
+    let total = {
+        list: [{
+            cartItem: {
+                item: new Item('ITEM000001', '雪碧', '瓶', 3.00),
+                count: 5
+            },
+            subtotal: 12.00,
+            itemDiscount: 3.00
+        },
+            {
+                cartItem: {
+                    item: new Item('ITEM000003', '荔枝', '斤', 15.00),
+                    count: 2
+                },
+                subtotal: 30.00,
+                itemDiscount: 0.00
+            },
+            {
+                cartItem: {
+                    item: new Item('ITEM000005', '方便面', '袋', 4.50),
+                    count: 3
+                },
+                subtotal: 9.00,
+                itemDiscount: 4.50
+            }
+        ],
+        total: 51.00,
+        discount: 7.50
+    };
+    let dateDigitToString = num => num < 10 ? `0${num}` : num;
+
+    it('should print correct text', () => {
+        const currentDate = new Date(),
+            year = dateDigitToString(currentDate.getFullYear()),
+            month = dateDigitToString(currentDate.getMonth() + 1),
+            date = dateDigitToString(currentDate.getDate()),
+            hour = dateDigitToString(currentDate.getHours()),
+            minute = dateDigitToString(currentDate.getMinutes()),
+            second = dateDigitToString(currentDate.getSeconds()),
+            formattedDateString = `${year}年${month}月${date}日 ${hour}:${minute}:${second}`;
+
+        const expectText = `***<没钱赚商店>收据***
+打印时间：${formattedDateString}
+----------------------
+名称：雪碧，数量：5瓶，单价：3.00(元)，小计：12.00(元)
+名称：荔枝，数量：2斤，单价：15.00(元)，小计：30.00(元)
+名称：方便面，数量：3袋，单价：4.50(元)，小计：9.00(元)
+----------------------
+总计：51.00(元)
+节省：7.50(元)
+**********************`;
+
+        expect(printReceiptTxt(total)).toEqual(expectText);
+    })
+})
