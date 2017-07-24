3bb3a28b273358a8db3c9346762270459235ce07
zhangge1998 <1263061442@qq.com>
2017-07-24 13:45:04 +0800

    test buildItemTotals

--- test/main-test.js
+++ test/main-test.js
@@ -30,3 +30,47 @@ describe('buildItems', ()=> {
         expect(buildItems(inputs)).toEqual(itemCounts);
     })
 });
+describe('buildItemTotals', ()=> {
+    let itemCounts = [
+        {
+            item: new Item('ITEM000001', '雪碧', '瓶', 3.00),
+            count: 5
+        },
+        {
+            item: new Item('ITEM000003', '荔枝', '斤', 15.00),
+            count: 2
+        },
+        {
+            item: new Item('ITEM000005', '方便面', '袋', 4.50),
+            count: 3
+        }
+    ];
+    it('should return right itemTotals', ()=> {
+        const itemTotals = [{
+            cartItem: {
+                item: new Item('ITEM000001', '雪碧', '瓶', 3.00),
+                count: 5
+            },
+            subtotal: 12.00,
+            itemDiscount: 3.00
+        },
+            {
+                cartItem: {
+                    item: new Item('ITEM000003', '荔枝', '斤', 15.00),
+                    count: 2
+                },
+                subtotal: 30.00,
+                itemDiscount: 0.00
+            },
+            {
+                cartItem: {
+                    item: new Item('ITEM000005', '方便面', '袋', 4.50),
+                    count: 3
+                },
+                subtotal: 9.00,
+                itemDiscount: 4.50
+            }
+        ];
+        expect(buildItemTotals(itemCounts)).toEqual(itemTotals);
+    })
+});
